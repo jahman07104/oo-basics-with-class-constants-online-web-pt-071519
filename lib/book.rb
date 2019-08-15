c16fc832ad14d29744878d2ce589d4637a6c8b76
Patrick Harrison <jahman@bellatlantic.net>
2019-08-15 19:07:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
+<<<<<<< HEAD
   attr_accessor :author, :page_count 
   attr_reader :title,:genre
+=======
+  attr_accessor :author, :page_count, 
+  attr_reader:genre
+>>>>>>> 5106c0f958a2250aa26f06b0c2cb5a685c40c8f0
   GENRES = []
 
   def initialize(title)
